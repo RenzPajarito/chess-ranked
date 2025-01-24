2164dfd99366545be59628963a3963d9103a5ff5
Renz Pajarito <85199262+RenzPajarito@users.noreply.github.com>
2025-01-24 20:41:11 +0800

    Create socket.ts

---
+++ src/pages/api/socket.ts
@@ -0,0 +1,40 @@
+import { Server } from 'socket.io';
+import type { Server as HTTPServer } from 'http';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { Socket as NetSocket } from 'net';
+
+interface SocketServer extends HTTPServer {
+  io?: Server | undefined;
+}
+
+interface SocketWithIO extends NetSocket {
+  server: SocketServer;
+}
+
+interface NextApiResponseWithSocket extends NextApiResponse {
+  socket: SocketWithIO;
+}
+
+const SocketHandler = (_: NextApiRequest, res: NextApiResponseWithSocket) => {
+  if (res.socket.server.io) {
+    res.end();
+    return;
+  }
+
+  const io = new Server(res.socket.server);
+  res.socket.server.io = io;
+
+  io.on('connection', (socket) => {
+    socket.on('joinGame', (gameId) => {
+      socket.join(gameId);
+    });
+
+    socket.on('move', ({ gameId, move }) => {
+      socket.to(gameId).emit('move', move);
+    });
+  });
+
+  res.end();
+};
+
+export default SocketHandler;
